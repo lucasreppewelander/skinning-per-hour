f8a6d886ed420339d180d64b9f21034966be0daf
Lucas Reppe Welander <lucaswelanderx@gmail.com>
2021-01-06 02:45:36 +0100

    Update SkinningPerHour.lua

--- SkinningPerHour/SkinningPerHour.lua
+++ SkinningPerHour/SkinningPerHour.lua
@@ -3,7 +3,8 @@ SkinningPerHour = { }
 -- skinning items
 _items = {172097, 172092, 172089, 172094, 172096}
 BAGS = {0, 1, 2, 3, 4}
--- GetContainerItemID(bag, slot) -- loop trough container and find the items above
+items = {}
+quantity = {}
 
 local function has_value (tab, val)
     for index, value in ipairs(tab) do
@@ -25,42 +26,26 @@ local function has_not_value (tab, val)
     return true
 end
 
-function SkinningPerHour:Initialize()
-    local background = "Interface\\TutorialFrame\\TutorialFrameBackground"
-
-    local f = CreateFrame("Frame",nil,UIParent)
-    f:SetFrameStrata("BACKGROUND")
-    f:SetWidth(400) -- Set these to whatever height/width is needed 
-    f:SetHeight(#_items * 23) -- for your Texture
-
-    local t = f:CreateTexture(nil,"BACKGROUND")
-    t:SetTexture(background)
-    t:SetAllPoints(f)
-    f.texture = t
-
-    f:SetPoint("TOPLEFT",10,-10)
-    return f
-end
-
 function SkinningPerHour:CheckItemsInBags(f)
-    items = {}
-
     for i=0, 4 do
         bagSlots = GetContainerNumSlots(i)
         slotsCheckLeft = bagSlots
-        slotIndex = 0
+        slotIndex = 1
 
         while ( slotsCheckLeft > 0 ) do
             itemID = GetContainerItemID(i, slotIndex)
 
             if has_value(_items, itemID) then
-                table.insert(items, itemID)
+                texture, itemCount = GetContainerItemInfo(i, slotIndex)
 
-                -- need to not append these to items if the already exists
-                -- but i do need to count them to get total amount of each item
-                -- that i currently have in my bags
+                if quantity[itemID] then
+                    quantity[itemID] = quantity[itemID] + itemCount
+                else
+                    quantity[itemID] = itemCount;
+                end
+
                 if has_not_value(items, itemID) then
-                    
+                    table.insert(items, itemID)
                 end
             end
 
@@ -69,7 +54,7 @@ function SkinningPerHour:CheckItemsInBags(f)
         end
     end
 
-    return items
+    return items, quantity
 end
 
 function SkinningPerHour:GetColorByName(quality)
@@ -90,29 +75,84 @@ function SkinningPerHour:GetColorByName(quality)
     end
 end
 
-function SkinningPerHour:PresentBagItems(i, itemID)
+function SkinningPerHour:PresentBagItems(i, itemID, quantity)
     local itemName = C_Item.GetItemNameByID(tostring(itemID))
     local quality = C_Item.GetItemQualityByID(tostring(itemID))
     local range = i * 20
     local negative = -range
     
-    local t = f:CreateFontString(f, "OVERLAY", "GameTooltipText")
-    t:SetPoint("TOPLEFT", 20, negative)
-    t:SetTextColor(SkinningPerHour:GetColorByName(quality))
+    f.fontStrings[itemID] = f:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+    f.fontStrings[itemID]:SetPoint("TOPLEFT", 20, negative)
+    f.fontStrings[itemID]:SetTextColor(SkinningPerHour:GetColorByName(quality))
 
     -- 163 53 238
 
-    t:SetText(itemName)
+    f.fontStrings[itemID]:SetText(itemName .. " (" .. quantity .. ")")
+end
+
+--[[
+function SkinningPerHour:RegisterEvent(self, event)
+    if event == "PLAYER_ENTERING_WORLD" then
+
+    else if event == "ITEM_PUSH" then
+        
+    end
+end
+]]
+
+function event__LOOT_READY(f)
+    local numItems = GetNumLootItems()
+    for slotID = 1, numItems do
+        local _, nme, qty = GetLootSlotInfo(slotID)
+        if (qty or 0) > 0 then
+            local _, link = GetItemInfo(nme)
+            local lootItemId = tonumber(string.match(link, "%d+"))
+
+            if has_value(_items, lootItemId) then
+                if quantity[lootItemId] then
+                    quantity[lootItemId] = quantity[lootItemId] + qty
+                else
+                    quantity[lootItemId] = qty;
+                end
+
+                if has_not_value(items, lootItemId) then
+                    table.insert(items, lootItemId)
+                end
+
+                f.fontStrings[lootItemId]:SetText(nme .. " (" .. quantity[lootItemId] .. ")")
+            end
+        end
+    end
 end
 
 function SkinningPerHour:Run()
-    f = SkinningPerHour:Initialize()
+    local background = "Interface\\TutorialFrame\\TutorialFrameBackground"
 
-    items = SkinningPerHour:CheckItemsInBags(f)
+    f = CreateFrame("Frame",nil,UIParent)
+    f:SetFrameStrata("BACKGROUND")
+    f:SetWidth(400) -- Set these to whatever height/width is needed 
+    f:SetHeight(#_items * 23) -- for your Texture
+
+    local t = f:CreateTexture(nil,"BACKGROUND")
+    t:SetTexture(background)
+    t:SetAllPoints(f)
+    f.texture = t
+
+    f:SetPoint("TOPLEFT",10,-10)
+    f.fontStrings = {}
+
+    items, quantity = SkinningPerHour:CheckItemsInBags(f)
 
     for index, value in pairs(items) do
-        SkinningPerHour:PresentBagItems(index, value)
-    end    
+        SkinningPerHour:PresentBagItems(index, value, quantity[value])
+    end
+
+    f:RegisterEvent("LOOT_OPENED")
+    f:SetScript("OnEvent", function(self, event, ...)
+        if event == "LOOT_OPENED" then
+            event__LOOT_READY(f)
+        end
+    end);
 
     f:Show()
 end
